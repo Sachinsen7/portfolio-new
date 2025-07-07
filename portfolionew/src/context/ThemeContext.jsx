3d52fedd0ef2a001bf9d879651627f416a0a1a85
Sachin Sen <sachinsen1920@gmail.com>
2025-07-08 01:58:56 +0530

    add dark mode functionality

---
+++ portfolionew/src/context/ThemeContext.jsx
@@ -0,0 +1,26 @@
+import { createContext, useState, useEffect } from "react";
+
+
+export const ThemeContext = createContext();
+
+
+// dark mode switcher
+
+export function ThemeProvider({children}){
+    const [theme, setTheme] = useState(localStorage.getItem("theme") || "light")
+
+    useEffect(() => {
+        document.documentElement.setAttribute("data-theme", theme)
+        localStorage.setItem("theme", theme)
+    }, [theme])
+
+    const toggleTheme = () => {
+        setTheme(theme === "light" ? "dark" : "light")
+    }
+
+    return (
+        <ThemeContext.Provider value={{theme, toggleTheme}}>
+            {children}
+        </ThemeContext.Provider>
+    )
+}
