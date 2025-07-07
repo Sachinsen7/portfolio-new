20b87ea77c8c7d72e5da55c0e8563c344bb8c9b7
Sachin Sen <sachinsen1920@gmail.com>
2025-07-08 02:21:59 +0530

    pages links

---
+++ portfolionew/src/lib/constants.js
@@ -0,0 +1,9 @@
+// Navigation links
+export const NAV_LINKS = [
+    { href: "#home", label: "Home" },
+    { href: "#about", label: "About" },
+    { href: "#projects", label: "Projects" },
+    { href: "#skills", label: "Skills" },
+    { href: "#blog", label: "Blog" },
+    { href: "#contact", label: "Contact" },
+  ];
