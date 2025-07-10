b0dc09686eb9f4533ab75d6ed0cd25555ba9cc18
Sachin Sen <sachinsen1920@gmail.com>
2025-07-10 19:36:21 +0530

    initialize animation

---
+++ portfolionew/src/components/common/AnimatedSection.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+import { useRef } from 'react'
+import { useIntersection } from '@/hooks/useIntersections'
+import { fadeIn } from '@/lib/animations'
+
+export default function AnimatedSection({children, className}) {
+    const ref = useRef(null)
+    const isVisible = useIntersection(ref)
+
+    return (
+        <motion.section
+          ref={ref}
+          initial="hidden"
+          animate={isVisible ? "visible" : "hidden"}
+          variants={fadeIn}
+          className={className}
+        >
+          {children}
+        </motion.section>
+    );
+  
+}
+
