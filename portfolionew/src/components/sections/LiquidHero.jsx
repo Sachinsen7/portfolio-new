aaebb84e974d01750d7db264a057488a90e70cfe
Sachin Sen <sachinsen1920@gmail.com>
2025-08-03 14:08:40 +0530

    Update LiquidHero with new profile photo

--- portfolionew/src/components/sections/LiquidHero.jsx
+++ portfolionew/src/components/sections/LiquidHero.jsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { Sparkles, Download, TwitterIcon, LinkedinIcon, GithubIcon, MailIcon } from "lucide-react";
-import profileImage from "/assets/images/me_and_bill.png";
+import profileImage from "/assets/images/edited-photo.png";
 import { basicSkills } from "@/lib/skillsData";
 
 export default function LiquidHero() {
