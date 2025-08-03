97da0d5c3397d35f5f1ba81eaa3e634cc5d1132f
Sachin Sen <sachinsen1920@gmail.com>
2025-08-03 14:05:43 +0530

    Add AnimatePresence for smooth page transitions

--- portfolionew/src/App.jsx
+++ portfolionew/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom'
+import { AnimatePresence } from 'framer-motion'
 import Home from '@/pages/Home'
 import AllProjects from '@/pages/AllProjects'
 import BlogPost from '@/pages/BlogPost'
@@ -9,18 +10,27 @@ import ProjectDetails from '@/components/sections/projectDetails'
 
 // Main app component with routing and theme provider
 
+function AnimatedRoutes() {
+  const location = useLocation();
+
+  return (
+    <AnimatePresence mode="wait" initial={false}>
+      <Routes location={location} key={location.pathname}>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/projects' element={<AllProjects/>}/>
+        <Route path='/project/:id' element={<ProjectDetails/>}/>
+        <Route path='/blog/:id' element={<BlogPost/>}/>
+        <Route path='*' element={<NotFound/>}/>
+      </Routes>
+    </AnimatePresence>
+  );
+}
+
 function App() {
   return (
     <ThemeProvider>
       <Router>
-        <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/projects' element={<AllProjects/>}/>
-          <Route path='/project/:id' element={<ProjectDetails/>}/>
-          <Route path='/blog/:id' element={<BlogPost/>}/>
-          <Route path='/projects' element={<AllProjects/>}/>
-          <Route path='*' element={<NotFound/>}/>
-        </Routes>
+        <AnimatedRoutes />
       </Router>
     </ThemeProvider>
   )
