891365da76fa8859d64ca59c0bdaf111d878ca23
Sachin Sen <sachinsen1920@gmail.com>
2025-10-23 11:58:58 +0530

    style(animations): Format code for consistency and readability

--- portfolionew/src/lib/animations.js
+++ portfolionew/src/lib/animations.js
@@ -1,9 +1,9 @@
 export const fadeIn = {
-    hidden: { opacity: 0, y: 20 },
-    visible: { opacity: 1, y: 0, transition: { duration: 0.8 } },
-  };
-  
-  export const scaleIn = {
-    hidden: { scale: 0.8, opacity: 0 },
-    visible: { scale: 1, opacity: 1, transition: { duration: 0.5 } },
-  };
+  hidden: { opacity: 0, y: 20 },
+  visible: { opacity: 1, y: 0, transition: { duration: 0.8 } },
+};
+
+export const scaleIn = {
+  hidden: { scale: 0.8, opacity: 0 },
+  visible: { scale: 1, opacity: 1, transition: { duration: 0.5 } },
+};
