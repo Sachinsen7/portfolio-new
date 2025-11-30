612bd395a12b3f78691d056187a090f971e768e4
Sachin Sen <sachinsen1920@gmail.com>
2025-11-30 19:49:46 +0530

    feat: Add navigation links constant.

--- portfolionew/src/lib/constants.js
+++ portfolionew/src/lib/constants.js
@@ -1,9 +1,8 @@
-// Navigation links
 export const NAV_LINKS = [
-    { href: "#home", label: "Home" },
-    { href: "#about", label: "About" },
-    { href: "#projects", label: "Projects" },
-    { href: "#skills", label: "Skills" },
-    { href: "#blog", label: "Blog" },
-    { href: "#contact", label: "Contact" },
-  ];
+  { href: "#home", label: "Home" },
+  { href: "#about", label: "About" },
+  { href: "#projects", label: "Projects" },
+  { href: "#skills", label: "Skills" },
+  { href: "#blog", label: "Blog" },
+  { href: "#contact", label: "Contact" },
+];
